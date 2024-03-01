c8fcbb2fced8524c0ace0c1185c3ab967ecfb5a2
Manuel <manuel.p80@gmx.de>
2024-03-01 20:34:11 +0100

    add-project structure

---
+++ HomeOfThings/Database.HotContext/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database.HotContext
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions opt):base(opt) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ HomeOfThings/Web.Core/Bundles/AppConfig.cs
@@ -0,0 +1,31 @@
+﻿using Database.HotContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web.Core.Bundles
+{
+    internal static class AppConfig
+    {
+        internal static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+        }
+
+        internal static void ConfigureDatabaseService(IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DatabaseContext>(opt =>
+            {
+                var connection = config.GetConnectionString("HotDataContext") ?? null;
+
+                if (connection == null)
+                {
+                    throw new InvalidOperationException("No database connection found!");
+                }
+                opt.UseMySQL(connection);
+
+            });
+        }
+    }
+}
